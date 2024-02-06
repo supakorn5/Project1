1b21bcfd6982f9d74a26dbb98405bba2112fe1f2
supakorn5 <64011212053@msu.ac.th>
2024-02-07 01:55:25 +0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:myproject/Wellcom/wellcom.dart';
+import 'package:myproject/Page/Wellcom/wellcom.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
